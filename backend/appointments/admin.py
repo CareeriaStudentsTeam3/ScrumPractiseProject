78a62666b44af92d10becf3a2f5e6b8a8739822b
Jasmin Alian <jasmin.alian@student.careeria.fi>
2021-03-25 20:59:39 +0200

    Create Models

--- backend/appointments/admin.py
+++ backend/appointments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Appointment)
+admin.site.register(models.Service)
+admin.site.register(models.Category)
+admin.site.register(models.Appointment_timespan)
+admin.site.register(models.Hairmodel)
