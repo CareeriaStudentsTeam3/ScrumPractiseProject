553d03b6967ce60a06b80f71b336cadbfd0c2d97
Jasmin Alian <jasmin.alian@student.careeria.fi>
2021-03-29 11:22:29 +0300

    Update settings.py

--- backend/appointment_app/settings.py
+++ backend/appointment_app/settings.py
@@ -127,5 +127,5 @@ AUTH_USER_MODEL = "appointments.User"
 STATIC_URL = '/static/'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
