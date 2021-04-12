d3015e6ffca92b10f3b012de3cff2115988473d6
Jasmin Alian <jasmin.alian@student.careeria.fi>
2021-04-12 12:10:09 +0300

    Add backend authentication

--- backend/appointments/views.py
+++ backend/appointments/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Hairmodel, Appointment_timespan, Appointment, Category, Service
 from django.db.models import F, ExpressionWrapper, DurationField
-from .serializers import HairModelSerializer, AppointmentTimeSpanSerializer, AppointmentSerializer, CategorySerializer, ServiceSerializer
+from .serializers import HairModelSerializer, AppointmentTimeSpanSerializer, AppointmentSerializer, CategorySerializer, ServiceSerializer, UserLoginSerializer
 from datetime import datetime,timedelta
+from django.contrib.auth import login, logout
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsAuthenticatedOrCreateOnly
 
 # Create your views here.
 
 class HairModelViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrCreateOnly]
     queryset = Hairmodel.objects.all()
     serializer_class = HairModelSerializer
 
 class AppointmentViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrCreateOnly]
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
 
 class AppointmentTimeSpanViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = AppointmentTimeSpanSerializer
     def get_queryset(self):
         queryset = Appointment_timespan.objects.all()
@@ -23,14 +35,18 @@ class AppointmentTimeSpanViewSet(viewsets.ModelViewSet):
         duration = self.request.query_params.get("duration")
         if group_size and duration is not None:
             group_size = int(group_size)
-            queryset = Appointment_timespan.objects.annotate(timediff=ExpressionWrapper(F("end")-F("beginning"), output_field=DurationField())).filter(max_group_size__gte=group_size, timediff=timedelta(minutes=int(duration)))
+            queryset = queryset.annotate(timediff=ExpressionWrapper(F("end")-F("beginning"), output_field=DurationField())).filter(max_group_size__gte=group_size, timediff=timedelta(minutes=int(duration)))
         return queryset
 
 class CategoryViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 class ServiceViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = ServiceSerializer
     def get_queryset(self):
         queryset = Service.objects.all()
@@ -39,4 +55,17 @@ class ServiceViewSet(viewsets.ModelViewSet):
             group_size = int(group_size)
             queryset = queryset.filter(max_group_size__gte=group_size)
         return queryset
-    
+
+class UserLoginView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        login(request, user)
+        return Response({"username": user.username, "login_success": True})
+
+class UserLogoutView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({"logout_success": True})
+
