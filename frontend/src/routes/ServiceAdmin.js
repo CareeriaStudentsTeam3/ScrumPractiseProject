f670899d198f28fee12a1fecfe48599b70a67cb2
Muukkonen <niko@muukkonen.info>
2021-04-14 12:54:51 +0300

    Add auth to service page

--- frontend/src/routes/ServiceAdmin.js
+++ frontend/src/routes/ServiceAdmin.js
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from 'react'
 
+// React-router-dom imports
+import { Redirect } from 'react-router-dom'
+
 // Component imports
 import ServiceList from '../components/admin/Service/ServiceList'
 import ServiceCreate from '../components/admin/Service/ServiceCreate'
+import LogoutButton from '../components/admin/LogoutButton/LogoutButton'
 
 // Service import
 import serviceService from '../services/service'
+import logoutService from '../services/logout'
 
 const ServiceAdmin = () => {
   const [services, setServices] = useState([])
 
   const [createService, setCreateService] = useState(false)
   const [refresh, setRefresh] = useState(false)
+  const [redirect, setRedirect] = useState(false)
 
   const addNewService = async (newService) => {
     const response = await serviceService.create(newService)
@@ -37,9 +43,32 @@ const ServiceAdmin = () => {
     setRefresh(false)
   }
 
+  // useEffect(() => {
+  //   getServices()
+  // }, [refresh])
+
   useEffect(() => {
-    getServices()
+    const loggedUserJSON = window.localStorage.getItem('user')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      if (user.login_success === true) {
+        // setUser(user.username)
+        getServices()
+        setRedirect(false)
+      }
+    }
+    if (
+      JSON.parse(loggedUserJSON) === null ||
+      !JSON.parse(loggedUserJSON).login_success
+    ) {
+      logoutService.logout()
+      setRedirect(true)
+    }
   }, [refresh])
+
+  if (redirect) {
+    return <Redirect to="/admin/login" />
+  }
 
   if (createService) {
     return (
@@ -52,6 +81,7 @@ const ServiceAdmin = () => {
 
   return (
     <div>
+      <LogoutButton />
       <ServiceList
         services={services}
         setCreateService={setCreateService}
