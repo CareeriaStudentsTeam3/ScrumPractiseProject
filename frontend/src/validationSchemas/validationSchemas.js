bc1cc09a6cbf6f43b8c0cbc2fe7e71395cee0bb5
Muukkonen <niko@muukkonen.info>
2021-04-28 10:12:15 +0300

    Add regex patterns for form validations

--- frontend/src/validationSchemas/validationSchemas.js
+++ frontend/src/validationSchemas/validationSchemas.js
@@ -1,4 +1,20 @@
 import * as yup from 'yup'
+// Only letters no whitespace no nothing no ä or ö
+const REGEX_ONLY_LETTERS = '^[a-zA-Z]+$'
+// For finland special chars ex. ö ä etc
+const REGEX_ONLY_LETTERS_ASCII = '^[A-Za-z\\u0080-\\uFFFF -]+$'
+// Only letters and whitespaces
+// const REGEX_ONLY_LETTERS_WHITESPACE = '^[a-zA-Z\\s]+$'
+const REGEX_ONLY_LETTERS_WHITESPACE_ASCII = '^[A-Za-z\\u0080-\\uFFFF - \\s]+$'
+
+// For phone numbers
+const REGEX_PHONE_NUMBER =
+  '^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$'
+
+const REGEX_NO_SPECIAL_CHAR = '^[A-Za-z\\u0080-\\uFFFF - \\s][^*<>$_]*$'
+
+// List of supported image formats
+const SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg', 'image/png']
 
 export const appointmentDateValidationSchema = yup.object({
   beginning: yup.string().required('Pakollinen'),
@@ -32,18 +48,22 @@ export const hairModelValidationSchema = yup.object({
   first_name: yup
     .string('Anna etunimi')
     .max(30, 'Etunimi saa olla enintään 30 merkkiä pitkä')
+    .matches(REGEX_ONLY_LETTERS_ASCII, 'Käytä pelkkiä kirjaimia')
     .required('Anna etunimi'),
   last_name: yup
     .string('Anna sukunimi')
     .max(50, 'Sukunimi saa olla enintään 50 merkkiä pitkä')
+    .matches(REGEX_ONLY_LETTERS_ASCII, 'Käytä pelkkiä kirjaimia')
     .required('Anna sukunimi'),
   city: yup
     .string('Anna kaupunki')
     .max(50, 'Kaupunki saa olla enintään 50 merkkiä pitkä')
+    .matches(REGEX_ONLY_LETTERS_ASCII, 'Käytä pelkkiä kirjaimia')
     .required('Anna kaupunki'),
   phone: yup
     .string('Anna puhelinnumero')
-    .max(13, 'Puhelinnumero saa olla enintään 13 merkkiä pitkä')
+    .max(12, 'Puhelinnumero saa olla enintään 12 merkkiä pitkä')
+    .matches(REGEX_PHONE_NUMBER, 'Tarkista puhelinnumeron muoto')
     .required('Anna puhelinnumero'),
   email: yup
     .string('Anna sähköpostiosoite')
@@ -55,18 +75,31 @@ export const hairModelValidationSchema = yup.object({
     .positive('Anna positiivinen luku')
     .integer('Anna kokoluku')
     .required('Anna ikä'),
-  gender: yup.string('Valitse sukupuoli').required('Valitse sukupuoli'),
+  gender: yup
+    .string('Valitse sukupuoli')
+    .matches(REGEX_ONLY_LETTERS, 'Käytä pelkkiä kirjaimia')
+    .required('Valitse sukupuoli'),
   hair_length: yup
     .string('Valitse hiustenpituus')
+    .matches(REGEX_ONLY_LETTERS, 'Käytä pelkkiä kirjaimia')
     .required('Valitse hiustenpituus'),
   hair_procedures: yup
     .string('Kerro edellisistä käsittelyistä')
     .max(200, 'Enintään 200 merkkiä')
+    .matches(
+      REGEX_NO_SPECIAL_CHAR,
+      'Tarkista ettei tekstissä ole kiellettyjä merkkejä'
+    )
     .required('Kerro edellisistä käsittelyistä'),
   image: yup
     .mixed()
     .nullable()
     .notRequired()
+    .test(
+      'FILE_TYPE',
+      'Sallitut kuva muodot .jpg .jpeg .png',
+      (value) => !value || SUPPORTED_FORMATS.includes(value.type)
+    )
     .test(
       'FILE_SIZE',
       'Kuvatiedosto on liian iso',
@@ -78,10 +111,12 @@ export const appointmentValidationSchema = yup.object({
   first_name: yup
     .string('Anna etunimi')
     .max(30, 'Etunimi saa olla enintään 30 merkkiä pitkä')
+    .matches(REGEX_ONLY_LETTERS_ASCII, 'Käytä pelkkiä kirjaimia')
     .required('Anna etunimi'),
   last_name: yup
     .string('Anna sukunimi')
     .max(50, 'Sukunimi saa olla enintään 50 merkkiä pitkä')
+    .matches(REGEX_ONLY_LETTERS_ASCII, 'Käytä pelkkiä kirjaimia')
     .required('Anna sukunimi'),
   email: yup
     .string('Anna sähköpostiosoite')
@@ -90,13 +125,19 @@ export const appointmentValidationSchema = yup.object({
     .required('Anna sähköpostiosoite'),
   phone: yup
     .string('Anna puhelinnumero')
-    .max(13, 'Puhelinnumero saa olla enintään 13 merkkiä pitkä')
+    .max(10, 'Puhelinnumero saa olla enintään 10 merkkiä pitkä')
+    .matches(REGEX_PHONE_NUMBER, 'Tarkista puhelinnumeron muoto')
     .required('Anna puhelinnumero'),
   place: yup
     .string('Anna paikka')
+    .matches(REGEX_ONLY_LETTERS_WHITESPACE_ASCII, 'Käytä pelkkiä kirjaimia')
     .required('Ilmoita haluttu paikka palvelulle'),
   info: yup
     .string('Anna lisätietoja')
     .max(200, 'Enintään 200 merkkiä')
+    .matches(
+      REGEX_NO_SPECIAL_CHAR,
+      'Tarkista ettei tekstissä ole kiellettyjä merkkejä'
+    )
     .required('Anna lisätietoja'),
 })
