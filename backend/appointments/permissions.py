ccbee99e6e1fa91df92398e69cbebb8422375ec6
Jasmin Alian <jasmin.alian@student.careeria.fi>
2021-05-24 21:52:39 +0300

    Fix permission issue

--- backend/appointments/permissions.py
+++ backend/appointments/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-
+from copy import deepcopy
 
 class CreateOnly(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -10,5 +10,6 @@ class CreateOnly(permissions.BasePermission):
 
 class CustomModelPerm(permissions.DjangoModelPermissions):
     def __init__(self):
+        self.perms_map = deepcopy(self.perms_map)
         self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
 
