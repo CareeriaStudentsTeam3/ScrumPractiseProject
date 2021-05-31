c43c1c688d23b79b2e4b99595166d84bae9db7e0
Muukkonen <niko@muukkonen.info>
2021-05-31 15:48:07 +0300

    Refactor HairModelUpdate

--- frontend/src/routes/HairdModelInfoAdmin.js
+++ frontend/src/routes/HairdModelInfoAdmin.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 // React-router-dom imports
-import { useParams, Redirect } from 'react-router-dom'
+import { useParams, Redirect, useHistory } from 'react-router-dom'
 
 // Service import
 import hairmodelService from '../services/hairmodel'
@@ -10,6 +10,7 @@ import logoutService from '../services/logout'
 // Component imports
 import HairModelUpdate from '../components/admin/HairModel/HairModelUpdate'
 import HairModelPhoto from '../components/admin/HairModel/HairModelPhoto'
+import Notification from '../components/Notification/Notification'
 
 // Material UI imports
 import Grid from '@material-ui/core/Grid'
@@ -17,10 +18,20 @@ import CircularProgress from '@material-ui/core/CircularProgress'
 
 const HairdModelInfoAdmin = () => {
   const { id } = useParams()
+  let history = useHistory()
 
   const [hairModel, setHairModel] = useState([])
   const [user, setUser] = useState(null)
   const [redirect, setRedirect] = useState(false)
+
+  // Notifications
+  const [open, setOpen] = useState(false)
+  const [notificationMsg, setNotificationMsg] = useState(null)
+
+  const handleNotification = (msg) => {
+    setOpen(true)
+    setNotificationMsg(msg)
+  }
 
   const getHairModel = async () => {
     setRedirect(false)
@@ -29,6 +40,97 @@ const HairdModelInfoAdmin = () => {
       return setRedirect(true)
     }
     setHairModel(response)
+  }
+
+  const handleSubmit = async (values) => {
+    let formData = new FormData()
+    formData.append('first_name', values.first_name)
+    formData.append('last_name', values.last_name)
+    formData.append('city', values.city)
+    formData.append('phone', values.phone)
+    formData.append('email', values.email)
+    formData.append('age', values.age)
+    formData.append('gender', values.gender)
+    formData.append('hair_length', values.hair_length)
+    formData.append('hair_procedures', values.hair_procedures)
+    if (values.image === null) {
+      console.log('inside formdata', values.image)
+      // formData.append('image', values.image)
+    }
+    if (values.image !== null && hairModel.image === null) {
+      formData.append('image', values.image)
+    }
+
+    console.log(values)
+    console.log(formData.get('image'))
+
+    try {
+      const response = await hairmodelService.update(formData, values.id)
+      console.log('res', response)
+      if (
+        response.detail === 'You do not have permission to perform this action.'
+      ) {
+        throw new Error('Sinulla ei ole oikeutta tehdä tätä!')
+      } else {
+        handleNotification('Tallennetaan...')
+        setTimeout(() => {
+          history.push({
+            pathname: '/admin/hairmodel',
+          })
+        }, 2000)
+      }
+    } catch (err) {
+      if (err.message.includes('Sinulla ei ole oikeutta tehdä tätä!')) {
+        handleNotification('Sinulla ei ole oikeutta tehdä tätä!')
+        setTimeout(() => {
+          setRedirect(true)
+        }, 3000)
+      } else {
+        handleNotification('On tapahtunut virhe! Palataan edelliselle sivulle.')
+        setTimeout(() => {
+          history.push({
+            pathname: '/admin/hairmodel',
+          })
+        }, 3000)
+      }
+    }
+  }
+
+  const handleDelete = async (id) => {
+    console.log(id)
+    if (window.confirm('Haluatko varmasti poistaa tämän hiusmallin?')) {
+      try {
+        const response = await hairmodelService.del(id)
+        if (
+          response.detail ===
+          'You do not have permission to perform this action.'
+        ) {
+          throw new Error('Sinulla ei ole oikeutta tehdä tätä!')
+        }
+        handleNotification('Poistetaan...')
+        setTimeout(() => {
+          history.push({
+            pathname: '/admin/hairmodel',
+          })
+        }, 2000)
+      } catch (err) {
+        if (err.message.includes('Sinulla ei ole oikeutta tehdä tätä!')) {
+          handleNotification('Sinulla ei ole oikeutta tehdä tätä!')
+          setTimeout(() => {
+            history.push({
+              pathname: '/admin/hairmodel',
+            })
+          }, 3000)
+        } else {
+          handleNotification('Hiusmallin poisto epäonnistui!')
+          setTimeout(() => {
+            history.push({
+              pathname: '/admin/hairmodel',
+            })
+          }, 3000)
+        }
+      }
+    }
   }
 
   useEffect(() => {
@@ -70,7 +172,14 @@ const HairdModelInfoAdmin = () => {
         <HairModelPhoto photo={hairModel.image} />
       </Grid>
       <Grid item xs={12} md={6}>
-        <HairModelUpdate hairModel={hairModel} user={user} />
+        <Notification message={notificationMsg} open={open} />
+        <HairModelUpdate
+          hairModel={hairModel}
+          user={user}
+          handleSubmit={handleSubmit}
+          handleDelete={handleDelete}
+          redirect={redirect}
+        />
       </Grid>
     </Grid>
   )
